df190f7bbd2c297f8890ac7e30e530ec9d9b38e7
taku-sami <takuya062090@gmail.com>
2021-08-23 14:14:37 +0900

    Change layout to 4key 2u inner

--- rev1/keymaps/mykeymap/keymap.c
+++ rev1/keymaps/mykeymap/keymap.c
@@ -22,7 +22,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Qwerty
+    /* Qwertý
      * ,----------------------------------------------------------------------------------------------------------------------.
      * | ESC  |   1  |   2  |   3  |   4  |   5  |   -  |                    |   =  |   6  |   7  |   8  |   9  |   0  |Pscree|
      * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -30,14 +30,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
      * |   5  |   A  |   S  |   D  |   F  |   G  |   `  |                    |   /  |   H  |   J  |   K  |   L  |   ;  |  "   |
      * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-     * | Shift|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |  UP  | Shift|
+     * | Shift|   Z  |   X  |   C  |   V  |   B  | Space| VAL+ |      | VAL- | Enter|   N  |   M  |   ,  |   .  |  UP  | Shift|
      * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-     * | Ctrl |  Tab |  GUI | Alt` |||||||| Lower| Space|  GUI |||||||| Bksp | Enter| Bksp ||||||||  GUI | Left | Down | Right|
+     * | Ctrl |  Tab |  Alt | Lower||||||||  GUI | Space|  Del |||||||| Bksp | Enter| Bksp ||||||||  GUI | Left | Down | Right|
      * ,----------------------------------------------------------------------------------------------------------------------.
      */
-    [_QWERTY] = LAYOUT(
+    [_QWERTY] = LAYOUT_4key_2u_inner(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_SYSTEM_SLEEP,
 
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PSCR, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_5, KC_A, KC_S, KC_D, KC_F, KC_G, KC_GRV, KC_SLSH, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_SPC, KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_RSFT, KC_LCTL, KC_TAB, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_LGUI, KC_BSPC, KC_ENT, KC_BSPC, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT),
+                                     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+
+                                     KC_5, KC_A, KC_S, KC_D, KC_F, KC_G, KC_GRV, KC_SLSH, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+
+                                     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC__VOLDOWN, KC__VOLUP, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_RSFT,
+
+                                     KC_LCTL, KC_TAB, KC_LALT, LOWER, KC_LGUI, KC_SPC, KC_DELETE, KC_BSPC, KC_ENT, KC_RGUI, ADJUST, KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* Lower
      * ,----------------------------------------------------------------------------------------------------------------------.
